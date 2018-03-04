3c4a35d70a942c25abb99d9d5109e518f65737fd
Yrjan Fraschetti <yrjanaff@gmail.com>
2018-03-04 12:55:16 +0100

    initial commit

---
+++ src/main/java/no/yaff/inneed/InneedApplication.java
@@ -0,0 +1,12 @@
+package no.yaff.inneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InneedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InneedApplication.class, args);
+	}
+}
